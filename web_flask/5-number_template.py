1fdf235c4dc69f46b2af2de6c272c63d17b10081
Amadu <amaduhamzajaarah300@gmail.com>
2023-04-22 12:25:46 -0700

    number templete

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-""" A script that starts a flask web application """
+#/usr/bin/python3
+"""  A script that starts a flask web application """
 from flask import Flask, render_template
 app = Flask(__name__)
 
